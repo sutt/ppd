059394e25f7ec897b89170d5ed78fd8e865519a4
Will Sutton Demo Krak <wsutton17@gmail.com>
2018-09-17 06:34:09 -0400

    adding timeseries graph to timelog interpret

--- timelog.py
+++ timelog.py
@@ -1,11 +1,17 @@
 import os
 import argparse
 from modules.Utils import TimeLog
+from matplotlib import pyplot as plt
+
+def showGraph(data):
+  plt.plot(data)
+  plt.show()
 
 ap = argparse.ArgumentParser()
 ap.add_argument("--file", default="data/sept2018/misc/output1.txt")
 ap.add_argument("--skip",  default=1)
 ap.add_argument("--hz",  action="store_true", default=False)
+ap.add_argument("--graph",  action="store_true", default=False)
 ap.add_argument("--filtervar", default="")
 ap.add_argument("--filterval", default="")
 args = vars(ap.parse_args())
@@ -20,8 +26,16 @@ timelog.interpret_log(
                       ,filter_schema_val =  args["filterval"]
                     )
 
+if args["graph"]:
+  data = timelog.load_log_data(path_fn=args["file"])
+  showGraph(data)
+
+
+
 #TODO - add tests
 
 def test_two_basic_output_files():
     ''' test these two vid time-per-frame mean are within each other CI '''
     pass
+
+
