815a6687266211cb7c0fc1b2ece25cf249b664e8
Will Sutton Demo Krak <wsutton17@gmail.com>
2018-09-28 16:13:09 -0400

    major refactor on roiRect and transform functions

--- modules/ControlDisplay.py
+++ modules/ControlDisplay.py
@@ -20,24 +20,40 @@ Features:
             [x] draw roi in main
         [x] zoom:blue, roi:yellow
         [ ] annotate with zoomwindow pixel size
-    
+            [ ] pixels in window, nums pixels in original
+        [ ] window3, diff
+        [ ] zoom on/off by gui
+        [x] crop from full size image
+        [ ] zero-modulo resize should be default under certain size zoomFrame
+
     [ ] orientation adjust
         [ ] handle flow thru of bounding box adjust
 
     [ ] Control Agenda box with keys
 
+Refactors:
+
+    [ ] remove hard coded dim size: 640, 320 etc.
+    [ ] document resize behavior
+    [x] transformRect - > absRect
+    [x] comment self data definitions
+    [ ] better explanation of alterFrame vs drawOperators
+    
     [ ] add unittests, can't test with guiview_test
         [ ] test for coord conversion between zoom and main
+        [ ] test an actual image for cutting pixels
+            [ ] show pixel blending on the border
+        [ ] test for resize behavior
 
 Bugs:
     [ ] adjust resize for correct aspect ratio
     [ ] how to cancel an roi request?
     [ ] can't exit on a zoom select request
     [ ] zoom frame is too large; do a max of those dims
-    [ ] crop zoom on full frame
+    [x] crop zoom on full frame
     [x] crop frame before annotations
     [x] need to erase previous drawn shapes in drawOperators
-    [ ] zoom isn't cropped after selectroi
+    [x] zoom isn't cropped after selectroi
 
 
 '''
@@ -55,12 +71,22 @@ class Display:
         self.zoomOn = False
         self.roiSelected = False
         
+        #this will preserved un-adulterated
+        self.origFrame = None
+        
+        #these will be potenitally resized, annotated, and drawn-on
+        # in alterFrames() and drawOperators()
         self.frame = None
         self.zoomFrame = None
 
+        #zoomRect in coord for (resized) frame; relative-rect format
         self.zoomRect = None
-        self.selectRectMain = None
-        self.selectRectZoom = None
+        
+        #roiRect in coords relative to origFrame; relative-rect format
+        self.roiRect = None
+        
+        self.selectRectMain = None  #needed this?
+        self.selectRectZoom = None  #needed this?
         
         self.annotateMsg = ""
         self.orientation = 0
@@ -68,6 +94,9 @@ class Display:
         self.cmdSelectZoom = False
         self.cmdSelectRoiMain = False
         self.cmdSelectRoiZoom = False
+
+        self.widthZoomWindow = 320
+        self.heightZoomWindow = 240
 
     def setOrientation(iOrientation):
         if iOrientation not in (90,180,270):
@@ -126,19 +155,17 @@ class Display:
             self.frame = draw_annotations(self.frame, self.annotateMsg)
 
         if self.orientation != 0:
-            pass    #rotate img
+            pass   
 
-        #new mainFrame, new zoomFrame
-        #could move to the top to avoid copying?
         self.zoomFrame = self.buildZoomFrame()
+        # self.alterZoomFrame()
 
-    def alterZoomrFrame(self, zoom_img):
+    def alterZoomFrame(self, zoom_img):
         ''' called after each new frame '''        
         
         # if self.frameResize:
         #     def.zoomFrame =  zoom_img = resize_img(self.frame, True, (640,480))
 
-        #TODO
         # if self.zoomAnnotateShape:
         #     self.frame = draw_annotations(self.frame, self.annotateMsg)
 
@@ -150,24 +177,32 @@ class Display:
         self.frame = self.getOrigFrame()
         self.alterFrame()
 
-        #TODO - reset zoomImg
         self.zoomFrame = self.buildZoomFrame()
+        # self.alterZoomFrame()
+
+        # self.drawOperators()
         
     
     def drawOperators(self):
-        ''' called within the cmd loop '''
+        ''' Draw onto frame(s) based on user input.
+            This has to be called for every new frame.
+            And everytime user/gui changes zoomRect or roiRect; resetOperators()
+        '''
 
-        #this only has to be called for a new frame, or reset operators
+        #TODO - return if not needed
 
         if self.zoomOn and self.zoomRect is not None:
+            
             self.frame = draw_rect(  self.frame
-                                    ,self.transformRect(self.zoomRect)
+                                    ,self.absRect(self.zoomRect)
                                     ,color='blue'
                                     ,thick = 1
                                     )
 
         if self.roiSelected:
-            x, y, radius = self.rectToCircle(self.roiRect)
+            
+            x, y, radius = self.rectToCircle(self.roiToMain())
+            
             self.frame = draw_circle(self.frame
                                     ,x
                                     ,y
@@ -175,15 +210,13 @@ class Display:
                                     ,color = 'yellow'
                                     ,thick = 3
                                     )
+            # because when mainwindow has resize, modulo might not be 0,
+            # so roiToMain in that case isn't a perfect match. But for 640 1280 1920, 
+            # they are perfect modulo, so it should be a match
 
         if self.roiSelected and self.zoomOn:
             
-            x0, y0, dx, dy = copy.copy(self.roiRect)
-            x0,y0 = self.mainToZoom((x0,y0))
-            x0,y0 = self.zoomCoordAdj(x0), self.zoomCoordAdj(y0)
-            dx, dy = self.zoomCoordAdj(dx), self.zoomCoordAdj(dy)
-            rectZoom = (x0, y0, dx, dy)
-            x, y, radius = self.rectToCircle(rectZoom)
+            x, y, radius = self.rectToCircle(self.roiToZoom())
             
             self.zoomFrame = draw_circle(self.zoomFrame
                                     ,x
@@ -192,6 +225,10 @@ class Display:
                                     ,color = 'yellow'
                                     ,thick = 1
                                     )
+            # because when zoomwindow has resize, modulo might not be 0,
+            # but whether it is or isn't, is decided in buildZoomFrame()
+            # with the choice of width
+    
     
     def buildZoomFrame(self):
         
@@ -202,24 +239,37 @@ class Display:
         
         else:
             
-            zoom_img = crop_img( self.getOrigFrameResize()
-                                ,self.transformRect(self.zoomRect)
+            zoom_img = crop_img( self.getOrigFrame()
+                                ,self.absRect(
+                                    tuple( 
+                                     self.scaleMainToOrig(p) for p in self.zoomRect
+                                        )
+                                    )
                                 )
-
-        #TODO - better logic, resize by min dimension reduction
+            
+            # this does not alter pixels, it is still directly derived
+            # from origFrame
         
-        zoom_img  = resize_img(zoom_img, True, (320,240))
+        widthZoom, heightZoom = self.widthZoomWindow,self.heightZoomWindow
+        
+        #TODO - adjust for modulo zoom
+        
+        zoom_img  = resize_img(zoom_img, True, (widthZoom, heightZoom))
 
-        # zoom_img = self.alterZooself.buildZoomFrame()mrFrame(zoom_img)
-        print zoom_img.shape
+        # since the zoom_img is always resize larger, if modulo == 0, then every pixel
+        # is (presumably?) scaled the exact same amount e.g. 3.0x increase has a 1 pixel 
+        # becoming a 3x3 square of the same data-values.
+        
         return zoom_img
 
+    
     def show(self):
-        ''' handle imshow calls, selectROI calls and return handling, and cvWindow
-            keypress handling
+        ''' Handle imshow calls, selectROI calls and return handling.
+                Also, cvWindow keypress handling.
+                
+            Note: need to keep entering this function once windows 
+                  are created or they become unresponsive.
         '''
-        # need to keep entering this function once windows are created or
-        # they become unresponsive
 
         if not(self.showOn):
             return
@@ -230,21 +280,18 @@ class Display:
 
         if self.cmdSelectRoiMain or self.cmdSelectZoom:
             
-            initBB = cv2.selectROI("img_display", self.frame, True, False )
-            
-            #note: this is based on resize on mainFrame, 640 is maxwidth
+            rect = cv2.selectROI("img_display", self.frame, True, False )
             
             if self.cmdSelectZoom:
-                self.zoomRect = initBB
+                self.zoomRect = rect
                 self.zoomOn = True
                 self.resetOperators()
 
             if self.cmdSelectRoiMain:
-                self.roiRect = initBB
+                self.roiRect = self.rectscaleMainToOrig(rect)
                 self.roiSelected = True
                 self.resetOperators()
         
-        # if self.zoomOn and self.zoomFrame is not None:
         if self.zoomOn:
         
             windowName = 'zoom_display'
@@ -254,17 +301,13 @@ class Display:
         if self.zoomOn and self.cmdSelectRoiZoom:
             
             rect = cv2.selectROI("zoom_display", self.zoomFrame, True, False )
-        
-            x0, y0, dx, dy = rect
 
-            x0,y0 = self.mainCoordAdj(x0), self.mainCoordAdj(y0)
-            dx, dy = self.mainCoordAdj(dx), self.mainCoordAdj(dy)
-            x0, y0 = self.zoomToMain((x0,y0))
-
-            self.roiRect = (x0, y0, dx, dy)
-            
+            self.roiRect = self.rectZoomToOrig(rect)
             self.roiSelected = True
             self.resetOperators()
+
+            # we don't need to ensure modulo-zero here, that has already been 
+            # enforced (or not) in the choice of zoomFrame-width in buildZoomFrame()
 
             
         #new func -----------
@@ -282,57 +325,148 @@ class Display:
         #         #TODO - add less zoom
 
     
-    #helpers ------
+    # main helpers ---------------------
+    # (roiRect (which is relative to origFrame coors) <-> to main/zoom)
 
-    def mainToZoom(self, xy):
-        ''' convert the coord is main window to the coord in zoom window '''
-        #account for orientation
+    def roiToMain(self):
+        ''' return relative-rect roi relative to main-window coord's.
+                (since there's no cropping, we only stretch.)
+                (we expect the stretch to be modulo-zero as 
+                 640 1280 1920 are multiple )
+        '''
+        rect = copy.copy(self.roiRect)
+        rectMain = tuple(map(self.scaleOrigToMain, rect))
+        return rectMain
+
+    def roiToZoom(self):
+        ''' return a relative-rect roi relative to zoom-window coord's.
+                (there's cropping and stretching.)
+                (we don't assume it is modulo-zero; but if it is, 
+                 this function should preserve that.)
+        '''
+        rect = copy.copy(self.roiRect)
         
-        x = xy[0] - self.zoomRect[0]
-        y = xy[1] - self.zoomRect[1]
+        #orig->main
+        x0, y0, dx, dy = map(self.scaleOrigToMain, rect)  
+        
+        #main->zoom
+        x0,y0 = self.coordMainToZoom((x0,y0))
+        x0 = self.scaleMainToZoom(x0)
+        y0 = self.scaleMainToZoom(y0)
+        dx = self.scaleMainToZoom(dx)
+        dy = self.scaleMainToZoom(dy)
+
+        rectZoom = (x0, y0, dx, dy)
+        
+        return rectZoom
+
+    def rectZoomToOrig(self, rectZoom):
+        ''' convert rect coords from relative to zoom to relative to orig
+        '''
+        x0, y0, dx, dy = rectZoom
+
+        #zoom->main
+        x0 = self.scaleZoomToMain(x0)
+        y0 = self.scaleZoomToMain(y0)
+        dx = self.scaleZoomToMain(dx)   
+        dy = self.scaleZoomToMain(dy)   
+        x0, y0 = self.coordZoomToMain((x0,y0))
+    
+        #main->orig
+        rectMain = (x0, y0, dx, dy)
+        rectOrig = tuple(map(self.scaleMainToOrig, rectMain))
+        
+        return rectOrig
+
+    def rectscaleMainToOrig(self, rectMain):
+        ''' convert rect coords from relative to main to relative to orig
+        '''
+        rectOrig = tuple(map(self.scaleMainToOrig, rectMain))
+        return rectOrig
+
+    
+    # helpers for the helpers: --------------
+    # (adjust individual coords or individual values)
+    def coordMainToZoom(self, coord_xy):
+        ''' convert the coord (a tuple of integers) in main window 
+            to the coord in zoom window '''
+        
+        #TODO - account for orientation
+        
+        x = coord_xy[0] - self.zoomRect[0]
+        y = coord_xy[1] - self.zoomRect[1]
 
         return x,y
 
-    def zoomCoordAdj(self, coord):
-
-        adj_factor = 320 / self.zoomRect[2] 
-
-        return int(coord*adj_factor)
-
-    def mainCoordAdj(self, coord):
-
-        adj_factor = self.zoomRect[2] / 320
-
-        return int(coord*adj_factor)
-
-
-    def zoomToMain(self, xy):
-        ''' convert the coord is main window to the coord in zoom window '''
-        #account for orientation
-        #TODO - implement
-        x = xy[0] + self.zoomRect[0]
-        y = xy[1] + self.zoomRect[1]
+    def coordZoomToMain(self, coord_xy):
+        ''' convert the coord (a tuple of integers) in zoom window 
+            to the coord in main window '''
+        
+        #TODO - account for orientation
+        
+        x = coord_xy[0] + self.zoomRect[0]
+        y = coord_xy[1] + self.zoomRect[1]
 
         return x,y
 
+    def scaleOrigToMain(self, val):
+        ''' convert val (an integer) from origFrame to (potentially resized) 
+            main window frame.
+                (assume proportional resize took place) 
+                (round nearest as we don't assume modulo-zero resize) '''
+        adj_factor = float(self.frame.shape[1]) / float(self.origFrame.shape[1])
+        adj_val = round(float(val)*adj_factor, 0)
+        return int(adj_val)
+
+    def scaleMainToOrig(self, val):
+        ''' convert val (an integer) from origFrame to (potentially resized) 
+            main window frame.
+                (assume proportional resize took place) 
+                (round nearest as we don't assume modulo-zero resize) '''
+        adj_factor = float(self.origFrame.shape[1]) / float(self.frame.shape[1])
+        adj_val = round(float(val)*adj_factor, 0)
+        return int(adj_val)
+
+    def scaleMainToZoom(self, val):
+        ''' a stretch transform: does rounding nearest for non modulo-zero '''
+        adj_factor = float(self.zoomFrame.shape[1]) / float(self.zoomRect[2])
+        adj_val = round(float(val)*adj_factor, 0)    
+        return int(adj_val)
+
+    def scaleZoomToMain(self, val):
+        ''' a stretch transform: does rounding nearest for non modulo-zero '''
+        adj_factor = float(self.zoomRect[2]) / float(self.zoomFrame.shape[1])
+        adj_val = round(float(val)*adj_factor,0)
+        return int(adj_val)
+    
+    # shape helpers ------------------
+    
     @staticmethod
-    def transformRect(input_rect):
-        ''' (x0,y0, d_x, d_y) -> ((xo,y0),(x1, y1)) 
-            note: must be tuples, not lists; to use in opencv functions
+    def absRect(input_rect):
+        ''' takes an (opencv style) relative rect, returns an absolute rect.
+                (x0,y0, d_x, d_y) -> ((xo,y0),(x1, y1)) 
+                note: must be tuples, not lists; to use in opencv functions
         '''
 
         x = copy.copy(input_rect)            
 
         rect = ( 
-                     ( int(x[0]),       int(x[1]) )
-                    ,( int(x[0] + x[2]), int(x[1] + x[3]) )
+                     (
+                         int(x[0])
+                        ,int(x[1]) 
+                     )
+                    ,( 
+                         int(x[0] + x[2])
+                        ,int(x[1] + x[3]) 
+                     )
                 )
 
         return rect
 
     @staticmethod
     def rectToCircle(input_rect):
-        ''' (x0,y0, d_x, d_y) -> (x,y, radius) 
+        ''' takes relative-format rect, fits a circle inside it
+            (x0,y0, d_x, d_y) -> (x,y, radius) 
             note: try to create a square for the sake of radius
         '''
 
@@ -388,7 +522,6 @@ if __name__ == "__main__":
         i += 1
         if i % 5*10**3 == 0:
             
-            print 'new rect box'
             display.rectZoom = (20,20, 40,40)   #debugZoom
         #     print 'cmdSelectZoom'
         #     display.setCmd(cmdSelectZoom=True)
@@ -422,3 +555,60 @@ def test_display_rectToCircle():
     assert y == 7 
     assert r == 5
 
+def test_display_zoom1():
+    ''' test how image resizing works '''
+    pass
+    
+    # assert img.shape == (320, 240)
+
+def test_display_zoom_crop_1():
+    ''' send picture data thru, see the transforms '''
+    
+    #test data
+    X,Y = 480,640
+    x0,x1 = 150,250
+    y0,y1= 300,400
+
+    rect = (x0,y0,x1,y1)
+    img = np.zeros(shape=(X,Y,3))
+    img = np.array(img, dtype=np.uint8)
+    for x in range(x0,x1):
+        for y in range (y0,y1):
+            img[x,y] = np.array([255,255,255] ,dtype=np.uint8)
+
+    #dont even need the actual image here; just go off roiRect
+
+    ZOOM = (100,100,200,200)
+    ROI = (150, 200, 120, 170)
+    
+    display = Display()
+    display.setInit(showOn=True)
+    display.setFrame(img)
+    display.zoomOn = True
+    display.zoomRect = ZOOM
+    display.roiRect = ROI
+    display.resetOperators()
+    
+    x0, y0, dx, dy = copy.copy(ROI)
+    x0,y0 = display.coordMainToZoom((x0,y0))
+    x0,y0 = display.scaleMainToZoom(x0), display.scaleMainToZoom(y0)
+    dx, dy = display.scaleMainToZoom(dx), display.scaleMainToZoom(dy)
+    rectZoom = (x0, y0, dx, dy)
+
+    display.drawOperators()
+    while(True):
+        display.show()
+
+    assert rectZoom == (1)
+
+if __name__ == "__main__":
+    test_display_zoom_crop_1()
+
+
+    #resizeIt
+    #cropIt 
+    #roiSelectIt, 
+    
+    # assert all([pix == (255,255,255)  for pix in cropped_region])
+
+    
